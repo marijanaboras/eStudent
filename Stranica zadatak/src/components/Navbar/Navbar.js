25464a5aa757315d134fa528d711f8b3a7947a89
Antonio <antonio.grigic1@gmail.com>
2020-10-08 23:03:26 +0200

    2

---
+++ Stranica zadatak/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '../Button/Button';
+import './Navbar.scss';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
